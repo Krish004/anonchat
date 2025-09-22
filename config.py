ce9361ae1bd47f1179c23e9e4ad206e4b86586cb
Krish004 <95554021+Krish004@users.noreply.github.com>
2025-09-22 15:45:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 class Config:
     # Telegram Bot
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", "12345"))   # optional if using BotToken only
-    API_HASH = os.environ.get("API_HASH", "api_hash_here")
+    API_ID = int(os.environ.get("API_ID", "1779071"))   # optional if using BotToken only
+    API_HASH = os.environ.get("API_HASH", "3448177952613312689f44b9d909b5d3")
 
     # Mongo
-    MONGO_URI = os.environ.get("MONGO_URI", "")
+    MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://tamilmovie:tamilmovie@cluster0.lxatf.mongodb.net/tamilmovie?retryWrites=true&w=majority")
     DB_NAME = os.environ.get("DB_NAME", "anon_chat")
 
     # Audit & Admin
@@ -20,4 +20,3 @@ class Config:
     # Deployment
     PORT = int(os.environ.get("PORT", "8080"))
     WEBHOOK = os.environ.get("WEBHOOK_URL", "")
-config.py
